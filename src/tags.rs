4443f65400167e2e8a84c8fc830529b7e571f8b0
Daniel Mueller <deso@posteo.net>
2018-07-30 08:27:58 -0700

    Make sure a 'complete' tag is always present
    With an upcoming change we would like to convert the existing task state
    (which currently just indicates whether a task has been completed or
    not) into a tag. This tag is sort of special, because it will be used by
    the UI directly, similarly to how the state field is used already
    nowadays. As such, it would be best if it were available unconditionally
    and would not need to be defined by the user.
    To that end, this change adds logic that adds this tag to the in-memory
    Templates object. If it was present already because it got persisted, no
    action will be performed. It also adds a reference to the template
    representing this tag directly into the Templates object. This way we
    can make sure not to have to search for this tag over and over again and
    have to unwrap the result but instead have guaranteed access.

--- src/tags.rs
+++ src/tags.rs
@@ -35,6 +35,9 @@ pub struct T(());
 
 pub type Id = IdT<T>;
 
+/// The name of a tag describing the completion state of a task.
+const COMPLETE_TAG: &str = "complete";
+
 
 /// A struct defining a particular tag.
 #[derive(Clone, Debug, Eq)]
@@ -44,6 +47,14 @@ pub struct Template {
 }
 
 impl Template {
+  /// Create a new tag template with the given name.
+  fn new(name: impl Into<String>) -> Self {
+    Template {
+      id: Id::new(),
+      name: name.into(),
+    }
+  }
+
   /// Convert the template into a serializable one.
   pub fn to_serde(&self) -> SerTemplate {
     SerTemplate {
@@ -137,11 +148,34 @@ impl Tag {
 pub type TagMap = BTreeMap<SerTagId, Id>;
 
 
+/// Ensure the given template set contains a tag with the given name.
+fn ensure_contains<S>(templates: &mut BTreeSet<Rc<Template>>, name: S) -> Rc<Template>
+where
+  S: Into<String> + AsRef<str>,
+{
+  let found = templates.iter().any(|x| x.name == name.as_ref());
+
+  if found {
+    templates
+      .iter()
+      .find(|x| x.name == name.as_ref())
+      .unwrap()
+      .clone()
+  } else {
+    let rc = Rc::new(Template::new(name.into()));
+    let inserted = templates.insert(rc.clone());
+    debug_assert!(inserted);
+    rc
+  }
+}
+
 /// A management structure for tag templates.
 #[derive(Clone, Debug, PartialEq)]
 pub struct Templates {
   /// A set of all the tag templates.
   templates: BTreeSet<Rc<Template>>,
+  /// Reference to the tag template representing task completion.
+  complete: Rc<Template>,
 }
 
 impl Templates {
@@ -155,7 +189,7 @@ impl Templates {
   /// The conversion also creates a "lookup" table mapping from the IDs
   /// as they were persisted to the in-memory ones.
   pub fn with_serde(mut templates: SerTemplates) -> (Self, TagMap) {
-    let (templates, map) = templates
+    let (mut templates, map) = templates
       .0
       .drain(..)
       .map(|x| {
@@ -166,8 +200,10 @@ impl Templates {
       })
       .unzip();
 
+    let complete = ensure_contains(&mut templates, COMPLETE_TAG);
     let templates = Templates {
       templates: templates,
+      complete: complete,
     };
     (templates, map)
   }
@@ -185,5 +221,47 @@ impl Templates {
   /// Convert the tag templates object into a serializable form.
   pub fn to_serde(&self) -> SerTemplates {
     SerTemplates(self.templates.iter().map(|x| x.to_serde()).collect())
+  }
+
+  /// Retrieve a reference to the 'complete' tag template.
+  #[cfg(test)]
+  pub fn complete_tag(&self) -> &Template {
+    &self.complete
+  }
+
+  /// Retrieve an iterator over all the tag templates.
+  #[cfg(test)]
+  pub fn iter(&self) -> impl Iterator<Item=&Template> {
+    self.templates.iter().map(|x| x.as_ref())
+  }
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  use serde_json::from_str as from_json;
+  use serde_json::to_string as to_json;
+
+
+  #[test]
+  fn ensure_complete_tag_exists() {
+    let templates = Templates::new();
+    let template = templates.iter().find(|x| x.name == COMPLETE_TAG).unwrap();
+    assert_eq!(template.id(), templates.complete_tag().id());
+  }
+
+  #[test]
+  fn ensure_complete_tag_is_not_duplicated() {
+    let templates = Templates::new();
+    let serialized = to_json(&templates.to_serde()).unwrap();
+    let deserialized = from_json::<SerTemplates>(&serialized).unwrap();
+    let (new_templates, _) = Templates::with_serde(deserialized);
+
+    let count = new_templates
+      .iter()
+      .fold(0, |c, x| if x.name == COMPLETE_TAG { c + 1 } else { c });
+    assert_eq!(count, 1);
   }
 }
