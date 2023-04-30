69ee8c81b61b949ffc44cc782fc976298857a862
Daniel Müller <deso@posteo.net>
2023-04-29 20:19:44 -0700

    Switch to using grev::git_revision() function in build.rs
    With the release of version 0.1.2 of the grev crate, the get_revision()
    function that we were relying on in our build script has been
    deprecated.
    With this change we replace its usage with the recommended
    git_revision() function instead.

--- build.rs
+++ build.rs
@@ -1,16 +1,14 @@
-// Copyright (C) 2022 Daniel Mueller (deso@posteo.net)
+// Copyright (C) 2022-2023 Daniel Mueller (deso@posteo.net)
 // SPDX-License-Identifier: GPL-3.0-or-later
-
-use std::io::stdout;
 
 use anyhow::Result;
 
-use grev::get_revision as get_git_rev;
+use grev::git_revision;
 
 
 fn main() -> Result<()> {
   let dir = env!("CARGO_MANIFEST_DIR");
-  if let Some(git_rev) = get_git_rev(dir, stdout().lock())? {
+  if let Some(git_rev) = git_revision(dir, ["src/"])? {
     println!(
       "cargo:rustc-env=NOTNOW_VERSION={} (@ {})",
       env!("CARGO_PKG_VERSION"),
