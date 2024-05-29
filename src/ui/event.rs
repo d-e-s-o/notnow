552969206e8f12b5baf8812aa116c4f821a5f992
Daniel Müller <deso@posteo.net>
2024-05-29 07:55:41 -0700

    Introduce Ids type for representing a set of ID objects
    We are working our way towards a world where the UI updates
    incrementally, only redrawing widgets that indicated that they changed.
    To make that vision a reality, we need a way to convey which widgets
    need to be updated from the event handling path.
    With this change we introduce the Ids type, which effectively is a set
    of ID objects representing the widgets to update. Two objects of this
    type can be merged, in which case the resulting object is the union of
    the IDs captured by both objects.

--- src/ui/event.rs
+++ src/ui/event.rs
@@ -1,11 +1,73 @@
 // Copyright (C) 2018-2024 Daniel Mueller (deso@posteo.net)
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use gui::Id;
 use gui::Mergeable;
 
 
 /// A key as used by the UI.
 pub use termion::event::Key;
+
+
+/// A type representing a set of `ID` objects, providing operations for
+/// merging sets.
+#[derive(Clone, Debug, PartialEq)]
+pub enum Ids<T = Id> {
+  One(T),
+  Two(T, T),
+  Three(T, T, T),
+  /// Any number of IDs.
+  ///
+  /// Note that the order is undefined.
+  Any(Vec<T>),
+}
+
+impl<T> Ids<T>
+where
+  T: Clone,
+{
+  pub fn merge_with(self, other: Self) -> Self {
+    match (self, other) {
+      (Ids::One(a), Ids::One(b)) => Ids::Two(a, b),
+      (Ids::One(a), Ids::Two(b, c)) => Ids::Three(a, b, c),
+      (Ids::One(a), Ids::Three(b, c, d)) => Ids::Any(vec![a, b, c, d]),
+      (Ids::One(a), Ids::Any(mut b)) => {
+        let () = b.extend_from_slice(&[a]);
+        Ids::Any(b)
+      },
+      (Ids::Two(a, b), Ids::One(c)) => Ids::Three(a, b, c),
+      (Ids::Two(a, b), Ids::Two(c, d)) => Ids::Any(vec![a, b, c, d]),
+      (Ids::Two(a, b), Ids::Three(c, d, e)) => Ids::Any(vec![a, b, c, d, e]),
+      (Ids::Two(a, b), Ids::Any(mut c)) => {
+        let () = c.extend_from_slice(&[a, b]);
+        Ids::Any(c)
+      },
+      (Ids::Three(a, b, c), Ids::One(d)) => Ids::Any(vec![a, b, c, d]),
+      (Ids::Three(a, b, c), Ids::Two(d, e)) => Ids::Any(vec![a, b, c, d, e]),
+      (Ids::Three(a, b, c), Ids::Three(d, e, f)) => Ids::Any(vec![a, b, c, d, e, f]),
+      (Ids::Three(a, b, c), Ids::Any(mut d)) => {
+        let () = d.extend_from_slice(&[a, b, c]);
+        Ids::Any(d)
+      },
+      (Ids::Any(mut a), Ids::One(b)) => {
+        let () = a.extend_from_slice(&[b]);
+        Ids::Any(a)
+      },
+      (Ids::Any(mut a), Ids::Two(b, c)) => {
+        let () = a.extend_from_slice(&[b, c]);
+        Ids::Any(a)
+      },
+      (Ids::Any(mut a), Ids::Three(b, c, d)) => {
+        let () = a.extend_from_slice(&[b, c, d]);
+        Ids::Any(a)
+      },
+      (Ids::Any(mut a), Ids::Any(mut b)) => {
+        let () = a.append(&mut b);
+        Ids::Any(a)
+      },
+    }
+  }
+}
 
 
 /// An event as used by the UI.
@@ -52,5 +114,27 @@ impl Mergeable for Event {
       (Self::Updated, Self::Updated) => self,
       (Self::Quit, _) | (_, Self::Quit) => Self::Quit,
     }
+  }
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+
+  /// Check that we can merge two `Ids` objects.
+  #[test]
+  fn ids_merging() {
+    let one1 = Ids::One(1);
+    let one2 = Ids::One(2);
+    let two = one1.merge_with(one2);
+    assert_eq!(two, Ids::Two(1, 2));
+
+    let three = two.merge_with(Ids::One(3));
+    assert_eq!(three, Ids::Three(1, 2, 3));
+
+    let any = three.merge_with(Ids::One(4));
+    assert_eq!(any, Ids::Any(vec![1, 2, 3, 4]));
   }
 }
