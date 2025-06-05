061a7d3c0ea028c31f3a9b8e962de256e297da74
Daniel Müller <deso@posteo.net>
2025-06-05 07:55:04 -0700

    Remove deprecated color configuration aliases
    Remove a bunch of aliases for color names that we have supported in the
    past but that have long been deprecated. At the very least with version
    0.3.10 of the crate, everybody should now be using the new names.

--- src/colors.rs
+++ src/colors.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2024 Daniel Mueller <deso@posteo.net>
+// Copyright (C) 2019-2025 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use serde::de::Error;
@@ -141,7 +141,6 @@ impl PartialEq for Color {
 }
 
 
-// TODO: Remove all aliases with next breaking release.
 #[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
 pub struct Colors {
   #[serde(default = "Color::color0")]
@@ -176,21 +175,21 @@ pub struct Colors {
   pub detail_dialog_bg: Color,
   #[serde(default = "Color::color0")]
   pub detail_dialog_fg: Color,
-  #[serde(alias = "dialog_bg", default = "Color::dark_white")]
+  #[serde(default = "Color::dark_white")]
   pub tag_dialog_bg: Color,
-  #[serde(alias = "dialog_fg", default = "Color::color0")]
+  #[serde(default = "Color::color0")]
   pub tag_dialog_fg: Color,
-  #[serde(alias = "dialog_selected_tag_fg", default = "Color::color15")]
+  #[serde(default = "Color::color15")]
   pub tag_dialog_selected_tag_fg: Color,
-  #[serde(alias = "dialog_selected_tag_bg", default = "Color::color240")]
+  #[serde(default = "Color::color240")]
   pub tag_dialog_selected_tag_bg: Color,
-  #[serde(alias = "dialog_tag_set_fg", default = "Color::bright_green")]
+  #[serde(default = "Color::bright_green")]
   pub tag_dialog_tag_set_fg: Color,
-  #[serde(alias = "dialog_tag_set_bg", default = "Color::dark_white")]
+  #[serde(default = "Color::dark_white")]
   pub tag_dialog_tag_set_bg: Color,
-  #[serde(alias = "dialog_tag_unset_fg", default = "Color::soft_red")]
+  #[serde(default = "Color::soft_red")]
   pub tag_dialog_tag_unset_fg: Color,
-  #[serde(alias = "dialog_tag_unset_bg", default = "Color::dark_white")]
+  #[serde(default = "Color::dark_white")]
   pub tag_dialog_tag_unset_bg: Color,
   #[serde(default = "Color::color0")]
   pub in_out_success_fg: Color,
