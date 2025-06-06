88dd82259a680073b2d5715d9ea7cdb2b97fbef1
Daniel Müller <deso@posteo.net>
2025-06-06 08:40:12 -0700

    Remove support for deprecated notnow.json config format
    Commit cf022c00070b ("Split up UI configuration") adjusted the
    configuration format, moving more "volatile" data into a separately
    managed file. Yet, we kept support for the earlier configuration format.
    With this change we drop support for this format to remove some old
    cruft from the code base. This commit effectively reverts 2e26bb197fa7
    ("Automate update of UI configuration").

--- src/view.rs
+++ src/view.rs
@@ -336,9 +336,6 @@ impl ToSerde for View {
     SerView {
       name: self.name.clone(),
       lits,
-      // We always set the value to false here. This way, if on-disk
-      // state still uses the deprecated format, we will force a save.
-      deprecated: false,
     }
   }
 }
